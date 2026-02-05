e0b47b08a952c4e9b71fbe07a34c12226ba67005
Tyson <tyson.gerhard@gmail.com>
2026-02-05 10:34:04 -0800

    Add XHR/JSONP polling transports to Socket.IO
    Expand the configured Socket.IO transports to include 'xhr-polling' and 'jsonp-polling' as fallbacks alongside 'websocket' and 'flashsocket'. This improves compatibility with clients or proxies that don't support native WebSocket. (Change in src/factory/socketio_factory.js)

--- src/factory/socketio_factory.js
+++ src/factory/socketio_factory.js
@@ -47,7 +47,7 @@ SocketIOFactory.prototype.instance = function(){
         this.sio.configure(function () {
             this.sio.set('heartbeat timeout', this.properties['gateway.socketio.timeout']);
             this.sio.set('heartbeat interval', this.properties['gateway.socketio.interval']);
-            this.sio.set('transports', ['websocket', 'flashsocket']);
+            this.sio.set('transports', ['websocket', 'xhr-polling', 'jsonp-polling', 'flashsocket']);
             this.sio.set('store', redisStore);
         }.bind(this));
 
